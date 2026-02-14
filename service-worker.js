b150e16d55b608a54228140ee0201f9e9301b099
cresxvv <mehmethamzaozcan7@gmail.com>
2026-02-14 17:04:24 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = "vm-forever-cache";
-const FILES = [
-  "index.html",
-  "manifest.json",
-  "service-worker.js"
-];
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
