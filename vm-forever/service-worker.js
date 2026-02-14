5e9bc9f8dc91b48653f507f41d567c98da969c3e
cresxvv <mehmethamzaozcan7@gmail.com>
2026-02-14 16:24:28 +0300

    Add files via upload

---
+++ vm-forever/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "vm-forever-cache";
+const FILES = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
